b37b4e69ef559280ab22a6419ef7e00066f85891
DOMLIANG <pandaqiang@gmail.com>
2021-07-12 10:20:28 +0800

    first commit

---
+++ migrations/2_deploy.js
@@ -0,0 +1,7 @@
+// migrations/2_deploy.js
+// SPDX-License-Identifier: MIT
+const ERC721PresetMinterPauserAutoId = artifacts.require("ERC721PresetMinterPauserAutoId");
+
+module.exports = function(deployer) {
+  deployer.deploy(ERC721PresetMinterPauserAutoId, "Koala NFT","NFT", "https://raw.githubusercontent.com/domliang/images/origin/blog/koalanft_");
+};
